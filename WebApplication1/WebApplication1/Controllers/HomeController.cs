1018aa7c658a293392e1da422103dbf9875029db
karin <karin@DESKTOP-G3TCD3F>
2019-07-18 21:06:42 +0300

    Done

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            // update student to the database
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            // delete student from the database whose id matches with specified id
+
+            return RedirectToAction("Index");
+        }
+    }
+}
